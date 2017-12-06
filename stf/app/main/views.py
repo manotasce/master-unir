704812492b27e802de9829316e1deaec2745743a
Cesar Orozco <cesar.orozco@hotmail.es>
2017-12-05 20:17:11 -0500

    Configuracion del repositorio

--- stf/app/main/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template,redirect,url_for,abort,flash,request,\
-    current_app,make_response
-
-from . import main
-
-@main.route('/',methods=['GET','POST'])
-def index():
-    return render_template('index.html')
